4e0f01527b829aadfd7352859fe5cd13aadbe96e
cuiyongguo <cuiyongguo@bignox.com>
2021-06-07 20:05:44 +0800

    初始化

---
+++ objc_main/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc_main
+//
+//  Created by adam on 2021/6/7.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
